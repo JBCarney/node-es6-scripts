3b2b87877a0dc2b722f1faf9039f50e361960fa8
Jason Carney <jason-carney@outlook.com>
2017-06-10 15:27:58 -0400

    initial commit

---
+++ practice.js
@@ -0,0 +1,23 @@
+// function shiftLeft(rotations, numOfShifts, array) {
+//         if (array !== [,,] && rotations >= numOfShifts) {
+//             for (var i = 0; i < numOfShifts; i++) {
+//                 if (i <= rotations) {
+//                     array.push(array.shift());
+//                 }
+//             }
+//         }
+//         return array;
+//     }
+// console.log(...shiftLeft(5, 4, [1,2,3,4,5]));
+
+// function* shiftLeft(num, array) {
+//     var out = [];
+//     for (item of array) {
+//         out.push(item);
+//         if (out.length === num) {
+//             num 
+//         }
+//     }
+// }
+
+// var shift = shiftLeft(n, ) 
